cdf7ecf2b8dc0e2da19dbc3b5d796db928fa4b8d
feoche <francois.eoche@corp.ovh.com>
2017-12-14 14:11:59 +0100

    feat(Bulk component): Add parameters to the extra function bound on bulk button clicking

--- client/components/telecom/telephony/bulkAction/telephony-bulk-action.component.controller.js
+++ client/components/telecom/telephony/bulkAction/telephony-bulk-action.component.controller.js
@@ -13,7 +13,7 @@ angular.module("managerApp").controller("telephonyBulkActionCtrl", function ($q,
 
     self.onBulkActionBtnClick = function () {
         if (self.onOpen && _.isFunction(self.onOpen())) {
-            self.onOpen();
+            self.onOpen()();
         }
         return $uibModal.open({
             templateUrl: "components/telecom/telephony/bulkAction/modal/telephony-bulk-action-modal.html",
