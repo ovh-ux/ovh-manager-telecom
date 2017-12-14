1186a8d7ab0521819f43c5a617bbe370cb0482d5
feoche <francois.eoche@corp.ovh.com>
2017-12-14 14:45:41 +0100

    wip(Line - Call filtering): Add bulk action to call filtering on line pages
    This task is on dev hold, need to resume this afterwards

--- client/app/telecom/telephony/line/calls/filtering/telecom-telephony-line-calls-filtering.controller.js
+++ client/app/telecom/telephony/line/calls/filtering/telecom-telephony-line-calls-filtering.controller.js
@@ -1,4 +1,4 @@
-angular.module("managerApp").controller("TelecomTelephonyLineCallsFilteringCtrl", function ($stateParams, $q, $timeout, Toast, ToastError, OvhApiTelephony) {
+angular.module("managerApp").controller("TelecomTelephonyLineCallsFilteringCtrl", function ($stateParams, $q, $timeout, Toast, ToastError, OvhApiTelephony, $translate, telephonyBulk) {
     "use strict";
 
     var self = this;
@@ -87,6 +87,53 @@ angular.module("managerApp").controller("TelecomTelephonyLineCallsFilteringCtrl"
             self.isInitializing = false;
         });
     }
+
+    self.bulkDatas = {
+        billingAccount: $stateParams.billingAccount,
+        serviceName: $stateParams.serviceName,
+        infos: {
+            name: "screen",
+            actions: [{
+                name: "screen",
+                route: "/telephony/{billingAccount}/screen/{serviceName}",
+                method: "PUT",
+                params: null
+            }, {
+                name: "options",
+                route: "/telephony/{billingAccount}/line/{serviceName}/options",
+                method: "PUT",
+                params: null
+            }]
+        }
+    };
+
+    self.getBulkParams = function () {
+        return {
+            outgoingScreenList: self.screen && self.screen.modified && self.screen.modified.outgoingScreenList,
+            incomingScreenList: self.screen && self.screen.modified && self.screen.modified.incomingScreenList
+        } || self.options && self.options.modified;
+    };
+
+    self.onBulkSuccess = function (bulkResult) {
+        // display message of success or error
+        telephonyBulk.getToastInfos(bulkResult, {
+            fullSuccess: $translate.instant("telephony_line_calls_filtering_bulk_all_success"),
+            partialSuccess: $translate.instant("telephony_line_calls_filtering_bulk_some_success", {
+                count: bulkResult.success.length
+            }),
+            error: $translate.instant("telephony_line_calls_filtering_bulk_error")
+        }).forEach(function (toastInfo) {
+            Toast[toastInfo.type](toastInfo.message, {
+                hideAfter: null
+            });
+        });
+
+        init();
+    };
+
+    self.onBulkError = function (error) {
+        Toast.error([$translate.instant("telephony_line_calls_filtering_bulk_on_error"), _.get(error, "msg.data")].join(" "));
+    };
 
     self.updateScreen = function (type) {
         self.screen.isLoading = true;
