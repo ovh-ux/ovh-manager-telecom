d8de71b33ca60a812337483a394797a33cba2c2b
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2017-12-18 10:10:15 +0100

    feat(bulk): PR feedback - replace tabulation by 4 spaces, add eof char

--- client/components/telecom/telephony/bulkAction/telephony-bulk-action-updated-services-container.factory.js
+++ client/components/telecom/telephony/bulkAction/telephony-bulk-action-updated-services-container.factory.js
@@ -1,25 +1,25 @@
 angular.module("managerApp").factory("telephonyBulkActionUpdatedServicesContainer", function () {
-	"use strict";
+    "use strict";
 
-	var self = this;
+    var self = this;
 
-	self._updatedServices = [];
+    self._updatedServices = [];
 
-	function clearUpdatedServices () {
-		self._updatedServices = []
-	}
+    function clearUpdatedServices () {
+        self._updatedServices = []
+    }
 
-	function storeUpdatedServices (data) {
-		self._updatedServices = self._updatedServices.concat(data);
-	}
+    function storeUpdatedServices (data) {
+        self._updatedServices = self._updatedServices.concat(data);
+    }
 
-	function getUpdatedServices () {
-		return self._updatedServices;
-	}
+    function getUpdatedServices () {
+        return self._updatedServices;
+    }
 
-	return {
-		clearUpdatedServices: clearUpdatedServices,
-		getUpdatedServices: getUpdatedServices,
-		storeUpdatedServices: storeUpdatedServices
-	};
+    return {
+        clearUpdatedServices: clearUpdatedServices,
+        getUpdatedServices: getUpdatedServices,
+        storeUpdatedServices: storeUpdatedServices
+    };
 });
