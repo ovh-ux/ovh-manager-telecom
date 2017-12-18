2ffd23185401be7fde42b36fe3596a645e465df1
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2017-12-18 14:26:04 +0100

    :sparkles: feat(bulk): adding call forwarding to bulk action

--- client/components/telecom/telephony/bulkAction/telephony-bulk-action.component.controller.js
+++ client/components/telecom/telephony/bulkAction/telephony-bulk-action.component.controller.js
@@ -12,6 +12,9 @@ angular.module("managerApp").controller("telephonyBulkActionCtrl", function ($q,
     ============================== */
 
     self.onBulkActionBtnClick = function () {
+        if (self.onOpen && _.isFunction(self.onOpen())) {
+            self.onOpen()();
+        }
         return $uibModal.open({
             templateUrl: "components/telecom/telephony/bulkAction/modal/telephony-bulk-action-modal.html",
             controller: "telephonyBulkActionModalCtrl",
