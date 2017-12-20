88ebebbfb043a9ce02c79a86f9ccb96cee245013
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2017-12-20 09:56:14 +0100

    :sparkles:feat(bulk): bulk action for changing feature type

--- client/app/telecom/telephony/alias/configuration/changeType/telecom-telephony-alias-configuration-changeType.controller.js
+++ client/app/telecom/telephony/alias/configuration/changeType/telecom-telephony-alias-configuration-changeType.controller.js
@@ -1,10 +1,10 @@
-angular.module("managerApp").controller("TelecomTelephonyAliasConfigurationChangeTypeCtrl", function ($scope, $q, $translate, $state, $stateParams, TelephonyMediator, Toast) {
+angular.module("managerApp").controller("TelecomTelephonyAliasConfigurationChangeTypeCtrl", function ($scope, $q, $translate, $state, $stateParams, TelephonyMediator, Toast, voipServiceTask, telephonyBulk) {
     "use strict";
 
     var self = this;
 
     self.number = null;
-
+    self.noCache = false;
     self.loading = {
         changing: false,
         line: true
@@ -41,7 +41,7 @@ angular.module("managerApp").controller("TelecomTelephonyAliasConfigurationChang
     function init () {
         self.loading.line = true;
 
-        return TelephonyMediator.getGroup($stateParams.billingAccount).then(function (group) {
+        return TelephonyMediator.getGroup($stateParams.billingAccount, self.noCache).then(function (group) {
             self.number = group.getNumber($stateParams.serviceName);
             return self.number;
         }).then(function () {
@@ -59,6 +59,7 @@ angular.module("managerApp").controller("TelecomTelephonyAliasConfigurationChang
             ];
 
             self.number.feature.startEdition();
+            self.noCache = false;
 
             return self.number;
         }).catch(function (error) {
@@ -76,6 +77,108 @@ angular.module("managerApp").controller("TelecomTelephonyAliasConfigurationChang
     });
 
     /* -----  End of INITIALIZATION  ------*/
+
+    /* ===========================
+    =            BULK            =
+    ============================ */
+
+    self.bulkDatas = {
+        billingAccount: $stateParams.billingAccount,
+        serviceName: $stateParams.serviceName,
+        infos: {
+            name: "configurationNumberChangeType",
+            actions: [{
+                name: "options",
+                route: "/telephony/{billingAccount}/number/{serviceName}/changeFeatureType",
+                method: "POST",
+                params: null
+            }]
+        }
+    };
+
+    self.getBulkParams = function () {
+        var data = {
+            featureType: self.number.feature.featureType
+        };
+
+        return data;
+    };
+
+    self.onBulkSuccess = function (bulkResult) {
+
+        // check if server tasks are all successful
+        self.checkServerTasksStatus(bulkResult.success).then(function (errorCount) {
+            if(errorCount > 0) {
+                Toast.warn([$translate.instant("telephony_alias_config_change_type_bulk_server_tasks_some_error")]);
+            }
+
+            telephonyBulk.getToastInfos(bulkResult, {
+                fullSuccess: $translate.instant("telephony_alias_config_change_type_bulk_all_success"),
+                partialSuccess: $translate.instant("telephony_alias_config_change_type_bulk_some_success", {
+                    count: bulkResult.success.length
+                }),
+                error: $translate.instant("telephony_alias_config_change_type_bulk_error")
+            }).forEach(function (toastInfo) {
+                Toast[toastInfo.type](toastInfo.message, {
+                    hideAfter: null
+                });
+            });
+
+            self.changeType();
+            self.noCache = true;
+            // reset initial values to be able to modify again the options
+            init();
+        }, function () {
+            Toast.error([$translate.instant("telephony_alias_config_change_type_bulk_server_tasks_all_error")]);
+        });
+    };
+
+    self.onBulkError = function (error) {
+        Toast.error([$translate.instant("telephony_alias_config_change_type_bulk_on_error"), _.get(error, "msg.data")].join(" "));
+    };
+
+    self.checkServerTasksStatus = function (updatedServices) {
+        self.loading.changing = true;
+
+        var deferred = $q.defer();
+        var promises = [];
+        var tasksAndServicesInfos = [];
+
+        _.forEach(updatedServices, function (service) {
+            var info = {
+                billingAccount: service.billingAccount,
+                serviceName: service.serviceName,
+                id: _.chain(service.values).map("value").filter(function (val) {
+                    return val.action === "changeType";
+                }).value()[0].taskId
+            };
+
+            tasksAndServicesInfos.push(info);
+        });
+
+        _.forEach(tasksAndServicesInfos, function (task) {
+            promises.push(voipServiceTask.startPolling(task.billingAccount, task.serviceName, task.id));
+        });
+
+        // return deferred.promise;
+        $q.all(promises).then(function (results) {
+            deferred.resolve();
+        }, function (errors) {
+            var nbTasksInError = _.sumBy(errors, function (e) {
+                return (e.status.toLowerCase() !== "done" && e.status.toLowerCase() !== "cancelled") ? 1 : 0;
+            });
+
+            if(nbTasksInError < errors.length) {
+                deferred.resolve(nbTasksInError);
+            } else {
+                deferred.reject();
+            }
+        });
+
+        return deferred.promise;
+    };
+
+    /* -----  End of BULK  ------ */
 
     init();
 
