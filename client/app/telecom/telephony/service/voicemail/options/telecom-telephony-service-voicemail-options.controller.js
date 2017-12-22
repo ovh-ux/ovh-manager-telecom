b76e9fe2df3ffaecf35c5e58b0f0b1f74db58cd4
feoche <francoiseoche@gmail.com>
2017-12-22 12:02:41 +0100

    feat(Line - Voicemail Options): Add bulk action to voicemail options on line pages

--- client/app/telecom/telephony/service/voicemail/options/telecom-telephony-service-voicemail-options.controller.js
+++ client/app/telecom/telephony/service/voicemail/options/telecom-telephony-service-voicemail-options.controller.js
@@ -1,4 +1,4 @@
-angular.module("managerApp").controller("TelecomTelephonyServiceVoicemailOptionsCtrl", function ($scope, $stateParams, $q, $translate, $timeout, ToastError, OvhApiTelephony, OvhApiMe) {
+angular.module("managerApp").controller("TelecomTelephonyServiceVoicemailOptionsCtrl", function ($scope, $stateParams, $q, $translate, $timeout, ToastError, OvhApiTelephony, OvhApiMe, telephonyBulk, Toast) {
     "use strict";
 
     var self = this;
@@ -238,6 +238,57 @@ angular.module("managerApp").controller("TelecomTelephonyServiceVoicemailOptions
         }).finally(function () {
             self.recordingForm.isUpdating = false;
         });
+    };
+
+
+    self.bulkDatas = {
+        billingAccount: $stateParams.billingAccount,
+        serviceName: $stateParams.serviceName,
+        infos: {
+            name: "settings",
+            actions: [{
+                name: "settings",
+                route: "/telephony/{billingAccount}/voicemail/{serviceName}/settings",
+                method: "PUT",
+                params: null
+            }]
+        }
+    };
+
+    self.getBulkParams = function () {
+        return {
+            audioFormat: self.notificationForm.audioFormat,
+            doNotRecord: self.recordingForm.doNotRecord,
+            forcePassword: self.recordingForm.forcePassword,
+            fromEmail: self.notificationForm.fromEmail,
+            fromName: self.notificationForm.fromName,
+            keepMessage: self.notificationForm.keepMessage,
+            redirectionEmails: self.settings.redirectionEmails
+        };
+    };
+
+    self.onBulkSuccess = function (bulkResult) {
+        // display message of success or error
+        telephonyBulk.getToastInfos(bulkResult, {
+            fullSuccess: $translate.instant("telephony_line_answer_voicemail_options_bulk_all_success"),
+            partialSuccess: $translate.instant("telephony_line_answer_voicemail_options_bulk_some_success", {
+                count: bulkResult.success.length
+            }),
+            error: $translate.instant("telephony_line_answer_voicemail_options_bulk_error")
+        }).forEach(function (toastInfo) {
+            Toast[toastInfo.type](toastInfo.message, {
+                hideAfter: null
+            });
+        });
+
+        // reset initial values to be able to modify again the options
+        OvhApiTelephony.Line().Lexi().resetAllCache();
+
+        init();
+    };
+
+    self.onBulkError = function (error) {
+        Toast.error([$translate.instant("telephony_line_answer_voicemail_options_bulk_on_error"), _.get(error, "msg.data")].join(" "));
     };
 
     // update notification options
