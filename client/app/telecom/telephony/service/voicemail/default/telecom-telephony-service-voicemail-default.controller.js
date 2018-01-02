27be3f8fdcaede53163746b0d2cd982c6f55baee
feoche <francoiseoche@gmail.com>
2018-01-02 15:31:56 +0100

    feat(Line - Default Voicemail): Add bulk action to default voicemail option on line pages

--- client/app/telecom/telephony/service/voicemail/default/telecom-telephony-service-voicemail-default.controller.js
+++ client/app/telecom/telephony/service/voicemail/default/telecom-telephony-service-voicemail-default.controller.js
@@ -1,4 +1,4 @@
-angular.module("managerApp").controller("TelecomTelephonyServiceVoicemailDefaultCtrl", function ($scope, $stateParams, $q, $timeout, $filter, ToastError, OvhApiTelephony) {
+angular.module("managerApp").controller("TelecomTelephonyServiceVoicemailDefaultCtrl", function ($scope, $stateParams, $q, $timeout, $filter, $translate, ToastError, OvhApiTelephony, telephonyBulk, Toast) {
     "use strict";
 
     var self = this;
@@ -83,4 +83,49 @@ angular.module("managerApp").controller("TelecomTelephonyServiceVoicemailDefault
     };
 
     init();
+
+    self.bulkDatas = {
+        billingAccount: $stateParams.billingAccount,
+        serviceName: $stateParams.serviceName,
+        infos: {
+            name: "settings",
+            actions: [{
+                name: "options",
+                route: "/telephony/{billingAccount}/line/{serviceName}/options",
+                method: "PUT",
+                params: null
+            }]
+        }
+    };
+
+    self.getBulkParams = function () {
+        return {
+            defaultVoicemail: self.options.defaultVoicemail
+        };
+    };
+
+    self.onBulkSuccess = function (bulkResult) {
+        // display message of success or error
+        telephonyBulk.getToastInfos(bulkResult, {
+            fullSuccess: $translate.instant("telephony_line_answer_default_voicemail_bulk_all_success"),
+            partialSuccess: $translate.instant("telephony_line_answer_default_voicemail_bulk_some_success", {
+                count: bulkResult.success.length
+            }),
+            error: $translate.instant("telephony_line_answer_default_voicemail_bulk_error")
+        }).forEach(function (toastInfo) {
+            Toast[toastInfo.type](toastInfo.message, {
+                hideAfter: null
+            });
+        });
+
+        // reset initial values to be able to modify again the options
+        OvhApiTelephony.Line().Lexi().resetAllCache();
+
+        init();
+    };
+
+    self.onBulkError = function (error) {
+        Toast.error([$translate.instant("telephony_line_answer_default_voicemail_bulk_on_error"), _.get(error, "msg.data")].join(" "));
+    };
+
 });
