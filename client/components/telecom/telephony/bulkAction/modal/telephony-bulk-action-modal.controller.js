0a030f306bb2d24bd91ea5134a8f6c9ce5634cb7
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2018-01-25 14:21:21 +0100

    fix(bulk): conflict

--- client/components/telecom/telephony/bulkAction/modal/telephony-bulk-action-modal.controller.js
+++ client/components/telecom/telephony/bulkAction/modal/telephony-bulk-action-modal.controller.js
@@ -1,4 +1,4 @@
-angular.module("managerApp").controller("telephonyBulkActionModalCtrl", function ($http, $filter, $translate, $uibModalInstance, modalBindings, telecomVoip) {
+angular.module("managerApp").controller("telephonyBulkActionModalCtrl", function ($http, $filter, $q, $translate, $uibModalInstance, modalBindings, telecomVoip) {
     "use strict";
 
     var self = this;
@@ -189,18 +189,15 @@ angular.module("managerApp").controller("telephonyBulkActionModalCtrl", function
             }).value();
 
             if (self.bindings.filterServices && _.isFunction(self.bindings.filterServices())) {
-                // filter function can be asynchronous
-                if (_.isFunction(self.bindings.filterServices()(allServices).then)) {
-                    self.bindings.filterServices()(allServices).then((updatedServices) => {
-                        allServices = updatedServices;
-                        completeServiceListDetails();
-                        self.loading.init = false;
-                    });
-                } else {
-                    allServices = self.bindings.filterServices()(allServices);
+                allServices = self.bindings.filterServices()(allServices);
+                var filterPromise = _.isFunction(allServices.then) ? allServices : $q.when(allServices);
+
+                filterPromise.then((filteredServices) => {
+                    allServices = filteredServices;
                     completeServiceListDetails();
                     self.loading.init = false;
-                }
+                });
+
             } else {
                 completeServiceListDetails();
                 self.loading.init = false;
