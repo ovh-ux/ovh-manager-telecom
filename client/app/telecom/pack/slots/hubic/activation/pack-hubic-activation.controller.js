13a1d0ea9039e44002fb60a33d96cb3554a44870
Sandor Daniel <daniel.sandor@corp.ovh.com>
2018-01-26 14:52:45 +0100

    :ambulance: fix(pack): code review fixes

--- client/app/telecom/pack/slots/hubic/activation/pack-hubic-activation.controller.js
+++ client/app/telecom/pack/slots/hubic/activation/pack-hubic-activation.controller.js
@@ -1,20 +1,16 @@
 angular.module("managerApp").controller("PackHubicActivationCtrl", function ($scope, $stateParams, OvhApiPackXdslHubic, ToastError) {
     "use strict";
 
-    var self = this;
+    this.ToastError = ToastError;
 
     this.$onInit = function () {
-        self.loading = true;
         self.hubicList = [];
-        return OvhApiPackXdslHubic.Aapi().query({ packId: $stateParams.packName }).$promise.then(
-            function (data) {
-                self.hubicList = data;
-            },
-            function (err) {
-                return new ToastError(err);
-            }
-        ).finally(function () {
-            self.loading = false;
+        return OvhApiPackXdslHubic.Aapi().query({
+            packId: $stateParams.packName
+        }).$promise.then((data) => {
+            this.hubicList = data;
+        }).catch((err) => {
+            this.ToastError(err);
         });
     };
 });
