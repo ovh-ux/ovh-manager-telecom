4c284011961860ec3a0d7b353673e4da74f344ae
Sandor Daniel <daniel.sandor@corp.ovh.com>
2018-01-26 16:22:44 +0100

    :ambulance: fix() : fix icons, missing text and pagination bug

--- client/app/telecom/orders/telecom-orders.controller.js
+++ client/app/telecom/orders/telecom-orders.controller.js
@@ -3,29 +3,18 @@ angular.module("managerApp").controller("TelecomOrdersCtrl", function (OvhApiXds
 
     const self = this;
 
-    self.ordersDetails = [];
+    self.ordersDetails = null;
     self.orders = [];
     self.perPage = PAGINATION_PER_PAGE;
-    self.currentPage = 0;
-
-    /*= =============================
-    =            EVENTS            =
-    ==============================*/
-
-    self.onTransformItemNotify = function (item) {
-        self.ordersDetails.push(item);
-    };
-
-    /* -----  End of EVENTS  ------*/
 
     /*= =====================================
     =            INITIALIZATION            =
     ======================================*/
 
     this.$onInit = function () {
-        self.loading = true;
         OvhApiXdslOrderFollowup.Aapi().query().$promise.then(function (result) {
             self.orders = result;
+
             result.forEach(function (access) {
                 if (access.lastTask) {
                     access.badgeClass = ORDER_STATUS[access.lastTask.status].class;
@@ -38,11 +27,8 @@ angular.module("managerApp").controller("TelecomOrdersCtrl", function (OvhApiXds
             // sort: first ERROR, then TODO, then DOING, then DONE
             self.orders = _.sortByOrder(self.orders, ["priority"], ["desc"]);
 
-            // paginate
-            self.ordersDetails = self.orders.slice(0, self.perPage);
-            self.loading = false;
-        }, function (err) {
-            self.loading = false;
+            self.ordersDetails = self.orders;
+        }).catch(function (err) {
             return new ToastError(err);
         });
     };
