0a53f781b6abbdd60a7bccd804cd1283a0615e06
Sandor Daniel <daniel.sandor@corp.ovh.com>
2018-01-30 08:42:04 +0100

    :tractor: chore(datagrid) : datagrid on administration summary bills

--- client/app/telecom/telephony/billingAccount/billing/summary/telecom-telephony-billing-account-billing-summary.controller.js
+++ client/app/telecom/telephony/billingAccount/billing/summary/telecom-telephony-billing-account-billing-summary.controller.js
@@ -1,21 +1,12 @@
 angular.module("managerApp").controller("TelecomTelephonyBillingAccountBillingSummaryCtrl", function ($q, $filter, $window, $timeout, $stateParams, $translate, TelephonyMediator, OvhApiTelephony, Toast) {
     "use strict";
 
-    var self = this;
+    const self = this;
 
     self.group = null;
-
-    self.loading = {
-        init: false
-    };
-
     self.consumptionSummary = null;
     self.consumptionDetails = {
-        raw: null,
-        paginated: null,
-        sorted: null,
-        orderBy: "",
-        orderDesc: true
+        raw: null
     };
 
     /*= ==============================
@@ -40,37 +31,13 @@ angular.module("managerApp").controller("TelecomTelephonyBillingAccountBillingSu
 
             self.consumptionDetails.raw = consumption.details;
             self.consumptionSummary = consumption.summary;
-
-            self.sortConsumption();
         });
     }
-
-    self.sortConsumption = function () {
-        self.consumptionDetails.sorted = $filter("orderBy")(
-            self.consumptionDetails.raw,
-            self.consumptionDetails.orderBy,
-            self.consumptionDetails.orderDesc
-        );
-    };
-
-    self.orderBy = function (by) {
-        if (self.consumptionDetails.orderBy === by) {
-            self.consumptionDetails.orderDesc = !self.consumptionDetails.orderDesc;
-        } else {
-            self.consumptionDetails.orderBy = by;
-        }
-        self.sortConsumption();
-    };
-
-    /* -----  End of HELPERS  ------*/
-
 
     /*= =====================================
     =            INITIALIZATION            =
     ======================================*/
-
-    function init () {
-        self.loading.init = true;
+    this.$onInit = function () {
 
         return TelephonyMediator.getGroup($stateParams.billingAccount)
             .then(function (group) {
@@ -81,14 +48,6 @@ angular.module("managerApp").controller("TelecomTelephonyBillingAccountBillingSu
             .catch(function (error) {
                 Toast.error([$translate.instant("telephony_group_billing_summary_consumptioninit_error"), (error.data && error.data.message) || ""].join(" "));
                 return $q.reject(error);
-            })
-            .finally(function () {
-                self.loading.init = false;
             });
-    }
-
-    /* -----  End of INITIALIZATION  ------*/
-
-    init();
-
+    };
 });
