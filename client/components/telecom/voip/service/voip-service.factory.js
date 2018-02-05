4ecdc51ed6f87f44bf1f5a3af646ae6671df0a06
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2018-02-05 15:27:24 +0100

    chore(): delete duplicates child functions in class VoipService

--- client/components/telecom/voip/service/voip-service.factory.js
+++ client/components/telecom/voip/service/voip-service.factory.js
@@ -82,27 +82,6 @@ angular.module("managerApp").factory("VoipService", function () {
         }
 
         /**
-         *  @description
-         *  Get the real feature type of a service. Useful to get trunk lines.
-         *
-         *  @return {String} The real feature type of the service.
-         */
-        getRealFeatureType () {
-            return this.isSipTrunk() ? "trunk" : this.featureType;
-        }
-
-        /**
-         *  @description
-         *  Helper that try to check if the service offer is trunk or not.
-         *
-         *  @return {Boolean}
-         */
-        isSipTrunk () {
-            var publicOfferName = _.get(this.getPublicOffer, "name");
-            return publicOfferName === "trunk" || _.get(publicOfferName.split("."), "[0]") === "trunk";
-        }
-
-        /**
          * @description
          * Helper that check if service public offer is valid
          *
