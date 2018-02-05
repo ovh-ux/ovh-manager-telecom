36a8c824881eb026b2d2b2f0d0bcc4d512eda509
feoche <francois.eoche@corp.ovh.com>
2018-02-05 15:03:23 +0100

    wip(Pack - Xdsl) Adding page in case of missing RIO in invalid XDSL orders

---
+++ client/app/telecom/pack/xdsl/missingRio/pack-xdsl-missing-rio.controller.js
@@ -0,0 +1,43 @@
+angular.module("managerApp").controller("PackXdslMissingRioCtrl", function ($scope, $stateParams, $translate, $q, $timeout, $filter, OvhApiXdsl, Toast) {
+    "use strict";
+    var self = this;
+
+    this.loading = true;
+
+    this.init = function () {
+        self.loading = true;
+
+        self.missingRioForm = {};
+
+        return OvhApiXdsl.Lexi().get({ xdslId: $stateParams.serviceName }).$promise.then(function (data) {
+            return data;
+        }, function (err) {
+            Toast.error(err);
+        }).then(function () {
+            self.loading = false;
+        });
+    };
+
+    /**
+     * Send Rio number
+     */
+    this.sendRio = function () {
+        self.loading = true;
+
+        return OvhApiXdsl.Lexi().updateInvalidOrMissingRio({
+            xdslId: $stateParams.serviceName
+        }, {
+            relaunchWithoutPortability: self.missingRioForm.portNumber,
+            rio: self.missingRioForm.rio
+        }).$promise.then(function () {
+            Toast.success($translate.instant("xdsl_missing-rio_sent"));
+            self.init();
+        }, function () {
+            Toast.error($translate.instant("xdsl_missing-rio_error"));
+        }).finally(function () {
+            self.loading = false;
+        });
+    };
+
+    this.init();
+});
---
+++ client/app/telecom/pack/xdsl/missingRio/pack-xdsl-missing-rio.js
@@ -0,0 +1,19 @@
+angular.module("managerApp").config(function ($stateProvider) {
+    "use strict";
+    $stateProvider.state("telecom.pack.xdsl.missing-rio", {
+        url: "/missingRio",
+        views: {
+            accessView: {
+                templateUrl: "app/telecom/pack/xdsl/missingRio/pack-xdsl-missing-rio.html",
+                controller: "PackXdslMissingRioCtrl",
+                controllerAs: "PackXdslMissingRio"
+            }
+        },
+        translations: [
+            "common",
+            "components",
+            "telecom/pack",
+            "telecom/pack/xdsl/missingRio"
+        ]
+    });
+});
