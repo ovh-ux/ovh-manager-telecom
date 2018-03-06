613ad0cb8e499aee5bb24fd790d1d48e45a8aab2
Florian Renaut <florian.renaut@corp.ovh.com>
2018-03-06 15:37:45 +0100

    fix(ui-router): fix telephony line fax routes.

--- client/app/telecom/telephony/line/fax/password/telecom-telephony-line-fax-password.js
+++ client/app/telecom/telephony/line/fax/password/telecom-telephony-line-fax-password.js
@@ -3,11 +3,11 @@ angular.module("managerApp").config(function ($stateProvider) {
     $stateProvider.state("telecom.telephony.line.fax.password", {
         url: "/password",
         views: {
-            "@lineView": {
+            "lineView@telecom.telephony.line": {
                 templateUrl: "app/telecom/telephony/line/fax/password/telecom-telephony-line-fax-password.html",
                 noTranslations: true
             },
-            "@faxPasswordView": {
+            "faxPasswordView@telecom.telephony.line.fax.password": {
                 templateUrl: "app/telecom/telephony/service/fax/password/telecom-telephony-service-fax-password.html",
                 controller: "TelecomTelephonyServiceFaxPasswordCtrl",
                 controllerAs: "PasswordCtrl"
