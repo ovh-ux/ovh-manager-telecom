7d9d1f22e4d78efb2395da5f2070f8935d47050d
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2018-03-22 09:18:10 +0100

    chore(ui-router): missing pack xdsl states

--- client/app/telecom/pack/xdsl/modem/dmz/pack-xdsl-modem-dmz.js
+++ client/app/telecom/pack/xdsl/modem/dmz/pack-xdsl-modem-dmz.js
@@ -3,7 +3,7 @@ angular.module("managerApp").config(function ($stateProvider) {
     $stateProvider.state("telecom.pack.xdsl.modem.dmz", {
         url: "/dmz",
         views: {
-            modemView: {
+            "modemView@telecom.pack.xdsl.modem": {
                 templateUrl: "app/telecom/pack/xdsl/modem/dmz/pack-xdsl-modem-dmz.html",
                 controller: "XdslModemDmzCtrl",
                 controllerAs: "DmzCtrl"
--- client/app/telecom/telephony/alias/administration/terminate/telecom-telephony-alias-administration-terminate.js
+++ client/app/telecom/telephony/alias/administration/terminate/telecom-telephony-alias-administration-terminate.js
@@ -4,7 +4,7 @@ angular.module("managerApp").config(function ($stateProvider) {
     $stateProvider.state("telecom.telephony.alias.administration.terminate", {
         url: "/terminate",
         views: {
-            "aliasView@telecom.telephony.alias.configuration": {
+            "aliasView@telecom.telephony.alias": {
                 templateUrl: "app/telecom/telephony/alias/administration/terminate/telecom-telephony-alias-administration-terminate.html",
                 controller: "TelecomTelephonyAliasAdministrationTerminateCtrl",
                 controllerAs: "AliasTerminateCtrl"
