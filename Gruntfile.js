40aa1820d7fa67c5eb9bdb84fa7a1433f19ec4b7
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-03-26 15:59:15 +0200

    chore(gruntfile): fix grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -622,11 +622,11 @@ module.exports = function (grunt) {
         less: {
             options: {
                 paths: [
-                    "node_modules",
                     "<%= yeoman.client %>/bower_components",
                     "<%= yeoman.client %>/app",
                     "<%= yeoman.client %>/components",
-                    "<%= yeoman.client %>/assets/styles"
+                    "<%= yeoman.client %>/assets/styles",
+                    "node_modules"
                 ],
                 plugins: [
                     require("less-plugin-remcalc")
