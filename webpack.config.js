cbbbcd3bf2aa392eb75bc335d9e2ce94fa389d81
jleveugle <joffrey.leveugle@corp.ovh.comt>
2018-09-05 10:05:24 +0200

    build(webpack): split entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
 
 function getEntries(startPath, filter) {
   if (!fs.existsSync(startPath)) {
@@ -8,16 +9,17 @@ function getEntries(startPath, filter) {
   }
 
   const files = fs.readdirSync(startPath);
-  let found = [];
+  let found = {};
 
   files.forEach((filename) => {
     const filePath = path.resolve(startPath, filename);
     const stat = fs.lstatSync(filePath);
 
     if (stat.isDirectory()) {
-      found = found.concat(getEntries(filePath, filter));
+      found = _.assign(found, getEntries(filePath, filter));
     } else if (filter.test(filename) && filename.search('.spec.') < 0 && filename.search('.mock.') < 0) {
-      found.push(path.resolve(startPath, filename));
+      const resolvedPath = path.resolve(startPath, filename);
+      found[_.camelCase(resolvedPath)] = resolvedPath;
     }
   });
 
@@ -38,12 +40,15 @@ const { config } = require('@ovh-ux/ovh-manager-webpack-toolkit')({
 /* eslint-enable */
 
 module.exports = merge(config, {
-  entry: {
+  entry: _.assign({
     main: path.resolve(__dirname, './client/app/index.js'),
-    telecom: getEntries(path.resolve(__dirname, 'client/app/telecom'), /\.js$/),
-    config: [path.resolve(__dirname, 'client/app/config/all.js'), path.resolve(__dirname, 'client/app/config/dev.js')],
-    components: getEntries(path.resolve(__dirname, 'client/components'), /\.js$/),
   },
+  getEntries(path.resolve(__dirname, 'client/app/telecom'), /\.js$/),
+  {
+    config: [path.resolve(__dirname, 'client/app/config/all.js'), path.resolve(__dirname, 'client/app/config/dev.js')],
+  },
+  getEntries(path.resolve(__dirname, 'client/components'), /\.js$/),
+  ),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
