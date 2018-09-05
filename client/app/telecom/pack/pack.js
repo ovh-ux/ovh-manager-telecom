5acf3c0c35ed1fc293e015ba9fbfef5430616874
Florian Renaut <florian.renaut@corp.ovh.com>
2018-09-05 15:25:33 +0200

    chore(webpack): fix slots ng-include

--- client/app/telecom/pack/pack.js
+++ client/app/telecom/pack/pack.js
@@ -1,3 +1,15 @@
+angular.module('managerApp').run(($templateCache) => {
+    // import templates required by ng-include
+    $templateCache.put("app/telecom/pack/slots/voipLine/pack-voipLine.html", require("./slots/voipLine/pack-voipLine.html"));
+    $templateCache.put("app/telecom/pack/slots/task/pack-task.html", require("./slots/task/pack-task.html"));
+    $templateCache.put("app/telecom/pack/slots/voipEcoFax/pack-voipEcoFax.html", require("./slots/voipEcoFax/pack-voipEcoFax.html"));
+    $templateCache.put("app/telecom/pack/slots/hubic/pack-hubic.html", require("./slots/hubic/pack-hubic.html"));
+    $templateCache.put("app/telecom/pack/slots/exchangeAccount/pack-exchangeAccount.html", require("./slots/exchangeAccount/pack-exchangeAccount.html"));
+    $templateCache.put("app/telecom/pack/slots/xdslAccess/pack-xdslAccess.html", require("./slots/xdslAccess/pack-xdslAccess.html"));
+    $templateCache.put("app/telecom/pack/slots/informations/pack-informations.html", require("./slots/informations/pack-informations.html"));
+    $templateCache.put("app/telecom/pack/slots/promotionCode/pack-promotionCode.html", require("./slots/promotionCode/pack-promotionCode.html"));
+    $templateCache.put("app/telecom/pack/slots/domain/pack-domain.html", require("./slots/domain/pack-domain.html"));
+});
 angular.module('managerApp').config(($stateProvider) => {
   $stateProvider.state('telecom.pack', {
     url: '/pack/:packName',
