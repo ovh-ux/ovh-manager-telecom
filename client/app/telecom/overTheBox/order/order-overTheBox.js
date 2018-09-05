bc6219716f39cee7777055679c0b7f3b79c7bfbb
Florian Renaut <florian.renaut@corp.ovh.com>
2018-09-05 15:58:12 +0200

    chore(webpack): fix otb ng-include

--- client/app/telecom/overTheBox/order/order-overTheBox.js
+++ client/app/telecom/overTheBox/order/order-overTheBox.js
@@ -1,3 +1,10 @@
+angular.module('managerApp').run(($templateCache) => {
+    // import templates required by ng-include
+    $templateCache.put("app/telecom/overTheBox/warning/overTheBox-warning-noPaymentMeans.html", require("../warning/overTheBox-warning-noPaymentMeans.html"));
+    $templateCache.put("app/telecom/overTheBox/warning/overTheBox-warning-notFound.html", require("../warning/overTheBox-warning-notFound.html"));
+    $templateCache.put("app/telecom/overTheBox/warning/overTheBox-warning-noSubscription.html", require("../warning/overTheBox-warning-noSubscription.html"));
+    $templateCache.put("app/telecom/overTheBox/warning/overTheBox-warning-deviceToLink.html", require("../warning/overTheBox-warning-deviceToLink.html"));
+});
 angular.module('managerApp').config(($stateProvider) => {
   $stateProvider.state('telecom.overTheBox-order', {
     url: '/overTheBox/order',
