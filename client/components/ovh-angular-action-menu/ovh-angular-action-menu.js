e5fa7e7de4f691f1f934bba527d239d61f22bfaf
Florian Renaut <florian.renaut@corp.ovh.com>
2018-09-13 17:40:09 +0200

    chore(webpack): eslint fix

--- client/components/ovh-angular-action-menu/ovh-angular-action-menu.js
+++ client/components/ovh-angular-action-menu/ovh-angular-action-menu.js
@@ -2,4 +2,3 @@ angular.module('managerApp').run(($translate, asyncLoader) => {
   asyncLoader.addTranslations(import(`ovh-angular-actions-menu/src/ovh-angular-actions-menu/translations/Messages_${$translate.use()}.xml`).then(x => x.default));
   $translate.refresh();
 });
-
