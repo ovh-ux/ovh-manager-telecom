519503021d8789c94dd7800ca3bf5b4e418ebe57
jleveugle <joffrey.leveugle@corp.ovh.com>
2018-09-17 10:33:32 +0200

    build(*): update config

--- webpack.config.js
+++ webpack.config.js
@@ -6,31 +6,31 @@ const _ = require('lodash');
 
 /* eslint-disable import/no-unresolved, import/no-extraneous-dependencies */
 const { config } = require('@ovh-ux/ovh-manager-webpack-toolkit')({
-  template: path.resolve(__dirname, './client/index.html'),
-  basePath: path.resolve(__dirname, './client'),
+  template: './client/index.html',
+  basePath: './client',
   lessPath: [
-    path.resolve('./client/app'),
-    path.resolve('./client/components'),
-    path.resolve('./node_modules'),
+    './client/app',
+    './client/components',
+    './node_modules',
   ],
   root: path.resolve(__dirname, './client/app'),
   assets: {
     files: [
-      { from: path.resolve(__dirname, './client/app/common/assets'), to: 'assets' },
-      { from: path.resolve(__dirname, './node_modules/angular-i18n'), to: 'angular-i18n' },
-      { from: path.resolve(__dirname, './client/**/*.html'), context: 'client' },
+      { from: './client/app/common/assets', to: 'assets' },
+      { from: './node_modules/angular-i18n', to: 'angular-i18n' },
+      { from: './client/**/*.html', context: 'client' },
     ],
   },
 });
 /* eslint-enable */
 
-const folder = 'client/app/telecom';
+const folder = './client/app/telecom';
 const bundles = {};
 
-fs.readdirSync('client/app/telecom').forEach((file) => {
+fs.readdirSync(folder).forEach((file) => {
   const stats = fs.lstatSync(`${folder}/${file}`);
   if (stats.isDirectory()) {
-    const jsFiles = glob.sync(`${folder}/${file}/**/*.js`, { absolute: true });
+    const jsFiles = glob.sync(`${folder}/${file}/**/*.js`);
     if (jsFiles.length > 0) {
       bundles[file] = jsFiles;
     }
@@ -39,10 +39,10 @@ fs.readdirSync('client/app/telecom').forEach((file) => {
 
 module.exports = merge(config, {
   entry: _.assign({
-    main: path.resolve(__dirname, './client/app/index.js'),
-    telecom: glob.sync('client/app/telecom/*.js', { absolute: true }),
-    components: glob.sync('client/components/**/*.js', { absolute: true }),
-    config: [path.resolve(__dirname, 'client/app/config/all.js'), path.resolve(__dirname, `client/app/config/${process.env.WEBPACK_SERVE ? 'dev' : 'prod'}.js`)],
+    main: './client/app/index.js',
+    telecom: glob.sync('./client/app/telecom/*.js'),
+    components: glob.sync('./client/components/**/*.js'),
+    config: ['./client/app/config/all.js', `./client/app/config/${process.env.WEBPACK_SERVE ? 'dev' : 'prod'}.js`],
   }, bundles),
   output: {
     path: path.resolve(__dirname, 'dist'),
