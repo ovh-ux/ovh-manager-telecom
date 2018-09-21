20b95e54081cdc33c2fb15f0c5e0f1829ebab134
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-21 15:34:17 +0200

    fix: remove empty lines

--- client/components/ovh-angular-line-diagnostics/ovh-angular-line-diagnostics.js
+++ client/components/ovh-angular-line-diagnostics/ovh-angular-line-diagnostics.js
@@ -1,4 +1,3 @@
-
 angular.module('managerApp').run(($translate, asyncLoader) => {
   asyncLoader.addTranslations(import(`ovh-angular-line-diagnostics/src/ovh-angular-line-diagnostics/translations/Messages_${$translate.use()}.xml`).then(x => x.default));
   $translate.refresh();
