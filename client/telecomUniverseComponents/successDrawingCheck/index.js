147617855876efdbf7d63c0295a4b41c0bf59d89
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-10-15 17:24:01 +0200

    refactor(components.success-drawing-check): update less

--- client/telecomUniverseComponents/successDrawingCheck/index.js
+++ client/telecomUniverseComponents/successDrawingCheck/index.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 
 import tucSuccessDrawingCheck from './successDrawingCheck.component';
 
+import './successDrawingCheck.less';
+
 export default angular
   .module('tucSuccessDrawingCheck', [])
   .component('tucSuccessDrawingCheck', tucSuccessDrawingCheck)
