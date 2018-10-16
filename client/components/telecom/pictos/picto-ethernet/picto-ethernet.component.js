5f7cc8cd42af5de0b41be656e25cfa9b7eafcdd9
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-10-16 14:01:02 +0200

    refactor(components.telecom.pictos): remove unused components

--- client/components/telecom/pictos/picto-ethernet/picto-ethernet.component.js
+++
@@ -1,8 +0,0 @@
-angular.module('managerApp').component('pictoEthernet', {
-  templateUrl: 'components/telecom/pictos/picto-ethernet/picto-ethernet.html',
-  bindings: {
-    strokeColor: '<strokeColor',
-    fillColor: '<fillColor',
-  },
-  controllerAs: 'PictoEthernet',
-});
