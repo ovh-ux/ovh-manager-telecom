8a6000859e20cb3cef75f2c82cf9864c75808b09
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2018-11-22 08:57:52 +0100

    refactor: moving file to new location

--- client/app/telecom/telephony/alias/configuration/records/telecom-telephony-alias-configuration-records.js
+++
@@ -1,7 +0,0 @@
-angular.module('managerApp').config(($stateProvider) => {
-  $stateProvider.state('telecom.telephony.alias.configuration.records', {
-    url: '/records',
-    abstract: true,
-    translations: ['.'],
-  });
-});
