a4f02e4aeedd7fcd3ddf5d60e9d460a2c6cbbc98
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2018-11-27 07:57:05 +0100

    feat(pack.xdsl.tasks): update modem tasks list tab
    ref: XDSL-5198

--- client/app/telecom/pack/xdsl/tasks/pack-xdsl-tasks.controller.js
+++ client/app/telecom/pack/xdsl/tasks/pack-xdsl-tasks.controller.js
@@ -2,10 +2,6 @@ angular.module('managerApp')
   .controller('XdslTasksCtrl', function ($scope, $stateParams, $translate, OvhApiXdsl, Toast, PAGINATION_PER_PAGE) {
     const self = this;
 
-    self.loaders = {
-      init: true,
-    };
-
     self.taskIds = [];
     self.tasks = [];
     self.serviceName = $stateParams.serviceName;
@@ -18,14 +14,18 @@ angular.module('managerApp')
     };
 
     self.getTasks = function () {
-      OvhApiXdsl.v6().getTasks({ xdslId: $stateParams.serviceName }).$promise.then(
+      self.isLoading = true;
+      self.taskIds = null;
+      return OvhApiXdsl.v6().getTasks({ xdslId: $stateParams.serviceName }).$promise.then(
         (taskIds) => {
           self.taskIds = taskIds.map(taskId => ({ id: taskId }));
         },
         (error) => {
           Toast.error([$translate.instant('an_error_occured'), error.data.message].join(' '));
         },
-      );
+      ).finally(() => {
+        self.isLoading = false;
+      });
     };
 
     self.transformItem = function (id) {
@@ -36,5 +36,9 @@ angular.module('managerApp')
             Toast.error([$translate.instant('an_error_occured'), error.data.message].join(' '));
           },
         );
+    };
+
+    self.refresh = function () {
+      self.getTasks();
     };
   });
