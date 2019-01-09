8180a4d612ceabcfd38a212a932221e152a07fdd
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2019-01-09 11:36:34 +0100

    feat(telephony): add week-end and bank holidays (#1200)
    ref: UXCT-19

--- client/app/telecom/telephony/telecom-telephony.constant.js
+++ client/app/telecom/telephony/telecom-telephony.constant.js
@@ -146,6 +146,10 @@
           '0032907',
         ],
       },
+      specialNumberPrefix: {
+        france: ['00338'],
+        belgium: ['0032800', '003278', '0032900', '003270'],
+      },
     })
     .constant('TELEPHONY_SERVICE', {
       validSoundExtensions: ['aiff', 'au', 'flac', 'ogg', 'mp3', 'wav', 'wma'],
