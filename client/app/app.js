9d652d5485122bd87ef1f9cf24cac17a18e8884e
JeremyDec <34273689+JeremyDec@users.noreply.github.com>
2019-01-25 15:13:36 +0100

    feat: use translations for oui-clipboard (#1262)
    ref: MANAGER-1868

--- client/app/app.js
+++ client/app/app.js
@@ -191,6 +191,7 @@ angular.module('managerApp', [
     $transitions,
     $translate,
     $translatePartialLoader,
+    ouiClipboardConfiguration,
     ouiCriteriaAdderConfiguration,
     ouiDatagridConfiguration,
     ouiFieldConfiguration,
@@ -202,6 +203,12 @@ angular.module('managerApp', [
     $translatePartialLoader.addPart('components');
 
     const removeHook = $transitions.onSuccess({}, () => {
+      _.set(ouiClipboardConfiguration, 'translations', {
+        copyToClipboardLabel: $translate.instant('common_clipboard_copy_to_clipboard'),
+        copiedLabel: $translate.instant('common_clipboard_copied'),
+        notSupported: $translate.instant('common_clipboard_not_supported'),
+      });
+
       _.set(ouiCriteriaAdderConfiguration, 'translations', {
         column_label: $translate.instant('common_criteria_adder_column_label'),
         operator_label: $translate.instant('common_criteria_adder_operator_label'),
