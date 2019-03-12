7d2caea721e4eede0ad24b079dc4d035b97f7fbe
Marie JONES <marie.jones@corp.ovh.com>
2019-03-12 17:36:40 +0100

    refactor(pack.ecofax): use finally hook
    ref: MBP-39

--- client/app/telecom/pack/slots/voipEcoFax/pack-voipEcoFax.controller.js
+++ client/app/telecom/pack/slots/voipEcoFax/pack-voipEcoFax.controller.js
@@ -35,9 +35,8 @@ export default class PackVoipEcoFaxCtrl {
     })
       .then(({ ecofaxes }) => {
         this.services = ecofaxes;
-        this.$scope.loaders.services = false;
-      },
-      () => {
+      })
+      .finally(() => {
         this.$scope.loaders.services = false;
       });
   }
