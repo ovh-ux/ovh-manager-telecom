9ab11bced907caf7ac4223d588f882b26a9809dc
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2019-08-09 12:23:54 +0200

    feat(xdsl.modem): add button to reconfigure voip line
    ref: UXCT-186

--- client/app/telecom/pack/xdsl/modem/reset/pack-xdsl-modem-reset.controller.js
+++ client/app/telecom/pack/xdsl/modem/reset/pack-xdsl-modem-reset.controller.js
@@ -28,6 +28,27 @@ angular.module('managerApp').controller('XdslModemResetCtrl', function ($statePa
     return $q.when(null);
   };
 
+  this.reconfigureVoip = function () {
+    if (_.isEmpty($stateParams.serviceName)) {
+      return TucToast.error($translate.instant('xdsl_modem_reset_an_error_ocurred'));
+    }
+    TucPackXdslModemMediator.setTask('reconfigureVoip');
+    OvhApiXdsl.Modem().v6().reconfigureVoip({
+      xdslId: $stateParams.serviceName,
+    }).$promise.then((result) => {
+      if (result.status === 'todo' || result.status === 'doing') {
+        TucPackXdslModemMediator.setTask('reconfigureVoip');
+      }
+      TucPackXdslModemMediator.disableCapabilities();
+      TucToast.success($translate.instant('xdsl_modem_reset_reconf_tel_success'));
+      return result;
+    }).catch((err) => {
+      TucToast.error($translate.instant('xdsl_modem_reconf_tel_an_error_ocurred'));
+      return $q.reject(err);
+    });
+    return $q.when(null);
+  };
+
   const init = function () {
     $scope.$on('pack_xdsl_modem_task_resetModem', (event, state) => {
       if (!state) {
